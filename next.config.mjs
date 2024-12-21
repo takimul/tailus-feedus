6d2693ce9f3f3ed0f8a28ffbd3dff7b9c16f13a5
Takimul <takimul3@gmail.com>
2024-12-22 02:42:23 +0600

    vercel fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,22 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: ["www.themealdb.com"],
+//   },
+// };
+
+// export default nextConfig;
+
+
+//update 1
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["www.themealdb.com"],
+    domains: ["www.themealdb.com"], // Allows Next.js to optimize images from this domain
   },
+  reactStrictMode: true,  // Enables React Strict Mode (optional, but recommended)
+  swcMinify: true,       // Uses SWC for minification, which is faster than Terser
 };
 
 export default nextConfig;
